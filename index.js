339c8f93d408868eec0a1866ebb94f21956b1ff5
Jonty <jontyleslie@gmail.com>
2025-09-23 13:59:10 +1000

    New cities

--- index.js
+++ index.js
@@ -62,7 +62,12 @@ async function run(city) {
 
   console.log(response);
 
-  await fetch(String(urls[city])) //1
+  const xValues = [];
+  const yValues = [];
+  const xValues2 = [];
+  const yValues2 = [];
+
+  await fetch(String(urls[0])) //1
     .then((response) => response.json()) //2
     .then((observations) => {
       console.log(observations);
@@ -73,8 +78,6 @@ async function run(city) {
       let ci = observations.data[0].temp; //3
       let ci2 = observations.data[0].temp_feels_like;
       let parent = document.querySelector('#placeholder2');
-      const xValues = [];
-      const yValues = [];
       const barColors = ["red", "green","blue","orange","brown"];
       
       console.log("jo test" + window.callbacks.show_line_ticks);
@@ -171,6 +174,7 @@ async function run(city) {
 	      //p.textContent = currentTime + ". Temp: " + observations.data[i].temp + "°C. Jonty's Temp: " + observations.data[i].temp_feels_like;
         
         yValues[i] = observations.data[i].temp;
+        //yValues[i] = observations.data[i].temp;
         
         if (observations.data[i].time === "undefined"){
           xValues[i] = "Not found";
@@ -236,7 +240,201 @@ async function run(city) {
 	      parent.appendChild(p);
       }
 
-      function addData(chart, label, newData) {
+     
+      }
+
+      
+    )
+    .then((temp) => {
+
+    }
+    );
+
+
+  await fetch(String(urls[1])) //1
+    .then((response) => response.json()) //2
+    .then((observations) => {
+      console.log(observations);
+      console.log(observations.data[0].temp);
+      console.log(observations.notice);
+      console.log("test");
+      
+      let ci = observations.data[0].temp; //3
+      let ci2 = observations.data[0].temp_feels_like;
+      let parent = document.querySelector('#placeholder2');
+      const barColors = ["red", "green","blue","orange","brown"];
+      
+      console.log("jo test" + window.callbacks.show_line_ticks);
+
+      for (let i = 0; i < 1; i++){
+        let ci = observations.data[i].temp; 
+
+        let p = document.createElement('p');
+        let timer = new Date(); 
+        let realtimer = new Date(observations.data[i].time);
+        console.log(realtimer);
+        let now = new Date();
+        let currentTime = realtimer.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+        console.log(currentTime.getHours);
+        console.log(`Current Time: ${currentTime}`);
+        let nozero = realtimer.getHours().toString();
+        let nonzero = nozero.replace(/^0+/, '');
+        console.log(nonzero+":"+realtimer.getMinutes());
+        let formatTime = nozero;
+        console.log("nozerotime = "+nozero.toString());
+        switch(nozero.toString()){
+          case "0": formatTime = "Midnight";
+            break;
+          case "1": formatTime = "1am";
+            break;
+          case "2": formatTime = "2am";
+            break;
+          case "3": formatTime = "3am";
+            break;
+          case "4": formatTime = "4am";
+            break;
+          case "5": formatTime = "5am";
+            break;
+          case "6": formatTime = "6am";
+            break;
+          case "7": formatTime = "7am";
+            break;
+          case "8": formatTime = "8am";
+            break;
+          case "9": formatTime = "9am";
+            break;
+          case "10": formatTime = "10am";
+            break;
+          case "11": formatTime = "11am";
+            break;
+          case "12": formatTime = "12pm";
+            break;
+          case "13": formatTime = "1pm";
+            break;
+          case "14": formatTime = "2pm";
+            break;
+          case "15": formatTime = "3pm";
+            break;
+          case "16": formatTime = "4pm";
+            break;
+          case "17": formatTime = "5pm";
+            break;
+          case "18": formatTime = "6pm";
+            break;
+          case "19": formatTime = "7pm";
+            break;
+          case "20": formatTime = "8pm";
+            break;
+          case "21": formatTime = "9pm";
+            break;
+          case "22": formatTime = "10pm";
+            break;
+          case "23": formatTime = "11pm";
+            break;
+          defualt: "Unknown time!";
+            break;
+        }
+        console.log("formatTime = "+formatTime);
+	      p.textContent = formatTime + ". Temp: " + observations.data[i].temp + "°C. Jonty's Temp: " + observations.data[i].temp_feels_like+"°C.";
+
+	      parent.appendChild(p);
+      }
+
+      for (let i = 24; i > -1; i--){
+        let ci = observations.data[i].temp; 
+        console.log(ci);
+        let p = document.createElement('p');
+        //let timer = new Date(observations.data[i].time);
+        let timer = new Date(); 
+        let realtimer = new Date(observations.data[i].time);
+        console.log(realtimer);
+        let now = new Date();
+        let currentTime = realtimer.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+        console.log(currentTime.getHours);
+        console.log(`Current Time: ${currentTime}`);
+        let nozero = realtimer.getHours().toString();
+        let nonzero = nozero.replace(/^0+/, '');
+        console.log(nonzero+":"+realtimer.getMinutes());
+	      //p.textContent = currentTime + ". Temp: " + observations.data[i].temp + "°C. Jonty's Temp: " + observations.data[i].temp_feels_like;
+        
+        yValues2[i] = observations.data[i].temp;
+        //yValues[i] = observations.data[i].temp;
+        
+        if (observations.data[i].time === "undefined"){
+          xValues[i] = "Not found";
+        }
+        else{
+          // xValues[i] = observations.data[i].time;
+          let formatTime = nozero;
+          switch(nozero.toString()){
+            case "0": formatTime = "Midnight";
+              break;
+            case "1": formatTime = "1am";
+              break;
+            case "2": formatTime = "2am";
+              break;
+            case "3": formatTime = "3am";
+              break;
+            case "4": formatTime = "4am";
+              break;
+            case "5": formatTime = "5am";
+              break;
+            case "6": formatTime = "6am";
+              break;
+            case "7": formatTime = "7am";
+              break;
+            case "8": formatTime = "8am";
+              break;
+            case "9": formatTime = "9am";
+              break;
+            case "10": formatTime = "10am";
+              break;
+            case "11": formatTime = "11am";
+              break;
+            case "12": formatTime = "12pm";
+              break;
+            case "13": formatTime = "1pm";
+              break;
+            case "14": formatTime = "2pm";
+              break;
+            case "15": formatTime = "3pm";
+              break;
+            case "16": formatTime = "4pm";
+              break;
+            case "17": formatTime = "5pm";
+              break;
+            case "18": formatTime = "6pm";
+              break;
+            case "19": formatTime = "7pm";
+              break;
+            case "20": formatTime = "8pm";
+              break;
+            case "21": formatTime = "9pm";
+              break;
+            case "22": formatTime = "10pm";
+              break;
+            case "23": formatTime = "11pm";
+              break;
+            defualt: "Unknown time!";
+              break;
+          };
+          xValues[i] = formatTime;
+        }
+        
+	      parent.appendChild(p);
+      }
+
+     
+      }
+
+      
+    )
+    .then((temp) => {
+
+    }
+    );
+
+   function addData(chart, label, newData) {
         chart.data.labels.push(label);
         chart.data.datasets.forEach((dataset) => {
             dataset.data.push(newData);
@@ -244,16 +442,14 @@ async function run(city) {
         chart.update();
       }
 
-      function removeData(chart) {
-        chart.data.labels.pop();
-        chart.data.datasets.forEach((dataset) => {
-            dataset.data.pop();
-        });
-        chart.update();
-      }
+      // function removeData(chart) {
+      //   chart.data.labels.pop();
+      //   chart.data.datasets.forEach((dataset) => {
+      //       dataset.data.pop();
+      //   });
+      //   chart.update();
+      // }
  
-
-
       const plugin = {
         id: 'customCanvasBackgroundColor',
         beforeDraw: (chart, args, options) => {
@@ -274,14 +470,27 @@ async function run(city) {
           datasets: [{
             label: "",
             //color: "white",
-            borderColor: "lightGrey",       // Line color
+            borderColor: "pink",       // Line color
             //backgroundColor: "black",   // Point color
             //pointBackgroundColor: "black",
             //pointBorderColor: "black",
             fill: true,
             backgroundColor: 'rgba(85, 85, 85, 1)',
             data: yValues
-          }]
+          },
+          {
+            label: "",
+            //color: "white",
+            borderColor: "lightGrey",       // Line color
+            //backgroundColor: "black",   // Point color
+            //pointBackgroundColor: "black",
+            //pointBorderColor: "black",
+            fill: true,
+            backgroundColor: 'rgba(85, 85, 85, 1)',
+            data: yValues2
+          }
+        
+          ]
         },
         options: {
           //backgroundColor: 'black',
@@ -302,13 +511,16 @@ async function run(city) {
           },
           scales: {
             x: {
+              
               title: false,
               ticks: {
                 color: 'rgba(190, 190, 190, 1)'
               }
             },
             y: {
+             
               ticks: {
+                //labels: yValues + "cdcd",
                 //display: false,
                 color: 'rgba(190, 190, 190, 1)'
               }
@@ -320,16 +532,9 @@ async function run(city) {
         });
         //Chart.defaults.global.defautFontColor = 'white';
 
-        removeData(myChart);
+        //removeData(myChart);
         myChart.destroy();
-        addData(myChart, yValues, xValues);
-      }
-      
-    )
-    .then((temp) => {
-
-    }
-    );
+        // addData(myChart, yValues, xValues);
 
 
   var z = document.createElement("h2");
@@ -380,6 +585,18 @@ async function run(city) {
   // printAddress();
 
 }
-run(0);
+
+
+document.getElementById("melb").addEventListener("click", function (e) {
+  runner(0);
+});
+
+function runner(city){
+  console.log("RUNNER");
+  location.reload;
+  run(city);
+}
+
+run(1);
 window.run = run;
 // location.reload();
